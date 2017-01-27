de6f689e3b27266beac0926d7fe26d8a2695906d
Sam Goddard <hello@samgoddard.co.uk>
2017-01-27 10:53:16 +0000

    working version with basic eyeglass example

--- sass-compile.js
+++ sass-compile.js
@@ -6,38 +6,15 @@ var fs = require('fs')
 var rootDir = __dirname;
 var assetsDir = path.join(rootDir, "assets");
 var outputFile = assetsDir + '/main.css';
-var importOnce = require('node-sass-import-once');
 
 var options = {
   file: assetsDir + '/main.scss',
-  outFile: outputFile,
-  importer: importOnce,
-  importOnce: {
-    index: false,
-    css: false,
-  }
+  outFile: outputFile
 };
 
 options.eyeglass = {
   // specifying root lets the script run from any directory instead of having to be in the same directory.
   root: rootDir,
-
-  // where assets are installed by eyeglass to expose them according to their output url.
-  // If not provided, assets are not installed unless you provide a custom installer.
-  buildDir: path.join(rootDir, "dist"),
-
-  assets: {
-    // prefix to give assets for their output url.
-    httpPrefix: "assets",
-
-    // Add assets except for js and sass files
-    // The url passed to asset-url should be
-    // relative to the assets directory specified.
-    sources: [
-      {directory: assetsDir, globOpts: { ignore: ["**/*.js", "**/*.scss"] }}
-    ]
-  },
-
   engines: {
     sass: sass
   }
@@ -46,7 +23,7 @@ options.eyeglass = {
 // Standard node-sass rendering of a single file.
 sass.render(eyeglass(options), function(error, result) {
    if (error) {
-    console.log(error.status); // used to be "code" in v2x and below
+    console.log(error.status);
     console.log(error.column);
     console.log(error.message);
     console.log(error.line);
